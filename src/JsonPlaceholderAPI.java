537b0e52c2f64513ec1bbce8b0609355e91c0cb7
Dmytro <dmytro.shvalikovskyi.ki.2020@lpnu.ua>
2023-10-16 19:14:17 +0300

    Final

--- src/JsonPlaceholderAPI.java
+++ src/JsonPlaceholderAPI.java
@@ -1,28 +1,155 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class JsonPlaceholderAPI {
-    public static void createObj() {
+    private static final String JSON_URL = "https://jsonplaceholder.typicode.com/users";
+    public static String createObj(String jsonObj) {
+        try {
+            URL url = new URL(JSON_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+            try(DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) { //Відправляємо дані на сервер
+                dos.writeBytes(jsonObj);
+                dos.flush();
+            }
+            int responseCode = connection.getResponseCode();
+            if(responseCode == 201) { //Успішний запит на сервер
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder(); //Зберігаємо відповідь від сервера
+                String line;
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+                in.close();
+                return response.toString();
+            }
+            else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static String updateObj(int userId, String updatedObj) {
+        try {
+            URL url = new URL(JSON_URL + "/" + userId);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Встановлення методу запиту на PUT
+            connection.setRequestMethod("PUT");
+            // Встановлення заголовків для JSON-даних
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            // Увімкнення можливості відправки даних PUT
+            connection.setDoOutput(true);
+            // Надсилання оновлених JSON-даних
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.writeBytes(updatedObj);
+                wr.flush();
+            }
+            // Отримання відповіді від сервера
+            int responseCode = connection.getResponseCode();
+            if (responseCode == 200) {
+                // Код 200 означає успішне оновлення об'єкта
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static boolean deleteObj(int userId) {
+        try {
+            URL url = new URL(JSON_URL + "/" + userId);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("DELETE");
 
+            int responseCode = connection.getResponseCode();
+            return (responseCode >= 200 && responseCode < 300);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static String getUsersInfo() {
+        try {
+            URL url = new URL(JSON_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+
+            StringBuilder result = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+            }
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+    public static String getUserInfo(int userId) {
+        try {
+            URL url = new URL(JSON_URL + "/" + userId);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+
+            StringBuilder result = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+            }
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+    public static String getUserInfo(String username) {
+        try {
+            URL url = new URL(JSON_URL + "/?username=" + username);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+
+            StringBuilder result = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+            }
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
     }
 
-    public static void updateObj() {
 
-    }
-
-    public static void deleteObj() {
-
-    }
-
-    public static void getUsersInfo() {
-
-    }
-
-    public static void getUserInfo(String username) {
-
-    }
-    public static void getUserInfo(int id) {
-
-    }
-
-    //Завдання 2
+//        Завдання 2 +
 //        Доповніть програму методом, що буде виводити всі коментарі до останнього поста певного користувача і записувати їх у файл.
 //
 //        https://jsonplaceholder.typicode.com/users/1/posts Останнім вважаємо пост з найбільшим id.
@@ -31,11 +158,50 @@ public class JsonPlaceholderAPI {
 //
 //        Файл повинен називатись user-X-post-Y-comments.json, де Х - id користувача, Y - номер посту.
 
-    public static void printAllComments(int user_id) {
+    public static void printAllComments(int userId) {
+        try {
+            URL postURL = new URL(JSON_URL + "/" + userId + "/posts/");
+            HttpURLConnection connection = (HttpURLConnection) postURL.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
 
+            StringBuilder posts = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+                String line;
+                while((line = reader.readLine()) != null) {
+                    posts.append(line).append("\n");
+                }
+            }
+            JsonParser parser = new JsonParser();
+            JsonArray postsArray = parser.parse(posts.toString()).getAsJsonArray();
+            if(postsArray.size() > 0) {
+                int lastPostId = postsArray.get(postsArray.size() - 1).getAsJsonObject().get("id").getAsInt();
+                URL commentsURL = new URL("https://jsonplaceholder.typicode.com/posts/" + lastPostId + "/comments");
+                HttpURLConnection commentConnection = (HttpURLConnection) commentsURL.openConnection();
+                commentConnection.setRequestMethod("GET");
+                commentConnection.setDoInput(true);
+
+                StringBuilder result = new StringBuilder();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(commentConnection.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line).append("\n");
+                    }
+                }
+                System.out.println("Коментарі до останнього посту: \n" + result);
+                try (FileWriter fileWriter = new FileWriter("user-" + userId + "-post-" + lastPostId + "-comments.json")) {
+                    fileWriter.write(result.toString());
+                }
+            }
+            else {
+                System.out.println("Пости відсутні");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-//    Завдання 3
+//    Завдання 3 +
 //    Доповніть програму методом, що буде виводити всі відкриті задачі для користувача з ідентифікатором X.
 //
 //            https://jsonplaceholder.typicode.com/users/1/todos.
@@ -43,7 +209,28 @@ public class JsonPlaceholderAPI {
 //    Відкритими вважаються всі задачі, у яких completed = false.
 
     public static void openUserTasks(int user_id) {
+        try {
+            URL url = new URL(JSON_URL + "/" + user_id + "/todos");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
 
+            StringBuilder todos = new StringBuilder();
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    todos.append(line).append("\n");
+                }
+            }
+            JsonParser parser = new JsonParser();
+            JsonArray tasks = parser.parse(todos.toString()).getAsJsonArray();
+            for(int i = 0; i < tasks.size(); i++) {
+                if(!tasks.get(i).getAsJsonObject().get("completed").getAsBoolean())
+                    System.out.println(tasks.get(i));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,57 @@
 //Завдання 1
 //        Програма повинна містити методи для реалізації наступного функціоналу:
 //
-//        створення нового об'єкта в https://jsonplaceholder.typicode.com/users.
+//        створення нового об'єкта в https://jsonplaceholder.typicode.com/users. +
 //        Можливо, ви не побачите одразу змін на сайті.
 //        Метод працює правильно, якщо у відповідь на JSON з об'єктом повернувся такий самий JSON,
 //        але зі значенням id більшим на 1, ніж найбільший id на сайті.
 //
-//        оновлення об'єкту в https://jsonplaceholder.typicode.com/users.
+//        оновлення об'єкту в https://jsonplaceholder.typicode.com/users. +
 //        Можливо, ви не побачите одразу змін на сайті.
 //        Вважаємо, що метод працює правильно, якщо у відповідь ви отримаєте оновлений JSON
 //        (він повинен бути таким самим, що ви відправили).
 //
-//        видалення об'єкта з https://jsonplaceholder.typicode.com/users.
+//        видалення об'єкта з https://jsonplaceholder.typicode.com/users. +
 //        Тут будемо вважати коректним результат - статус відповіді з групи 2xx (наприклад, 200).
 //
-//        отримання інформації про всіх користувачів https://jsonplaceholder.typicode.com/users
+//        отримання інформації про всіх користувачів https://jsonplaceholder.typicode.com/users +
 //
-//        отримання інформації про користувача за id https://jsonplaceholder.typicode.com/users/{id}
+//        отримання інформації про користувача за id https://jsonplaceholder.typicode.com/users/{id} +
 //
-//        отримання інформації про користувача за username - https://jsonplaceholder.typicode.com/users?username={username}
+//        отримання інформації про користувача за username - https://jsonplaceholder.typicode.com/users?username={username} +
 
 public class Main {
     public static void main(String[] args) {
+
         //Завдання 1
-        JsonPlaceholderAPI.createObj();
-        JsonPlaceholderAPI.updateObj();
-        JsonPlaceholderAPI.deleteObj();
-        JsonPlaceholderAPI.getUsersInfo();
-        JsonPlaceholderAPI.getUserInfo(1);
-        JsonPlaceholderAPI.getUserInfo("username");
+        String jsonInput = "{"
+                + "\"name\": \"Dmytro\","
+                + "\"username\": \"Shvalikovskyi\","
+                + "\"email\": \"dmytro.shvalikovskyi.ki.2020@lpnu.ua\""
+                + "}";
+
+        String createdUser = JsonPlaceholderAPI.createObj(jsonInput);
+        System.out.println("\nСтворено нового користувача: \n" + createdUser);
+        String updatedObj = "{"
+                + "\"name\": \"Updated name\","
+                + "\"username\": \"Updated username\","
+                + "\"email\": \"updatedEmail@gmail.com\""
+                + "}";
+        int userId = 10;
+        String username = "Antonette";
+        String updatedUser = JsonPlaceholderAPI.updateObj(userId, updatedObj);
+        System.out.println("\nОновлений користувач: \n" + updatedUser);
+        System.out.println(String.format("\nРезультат видалення об'єкта з id %d: %b", userId, JsonPlaceholderAPI.deleteObj(userId)));
+        System.out.println(String.format("\nІнформація про користувачів:\n %s", JsonPlaceholderAPI.getUsersInfo()));
+        System.out.println(String.format("Інформація про користувача з id %d:\n %s", userId, JsonPlaceholderAPI.getUserInfo(userId)));
+        System.out.println(String.format("Інформація про користувача за username [%s]:\n %s", username, JsonPlaceholderAPI.getUserInfo(username)));
+
         //Завдання 2
-        JsonPlaceholderAPI.printAllComments(1);
+        System.out.println(String.format("Перевірка завдання 2\nВивід коментарів до останнього поста користувача з id %d", userId));
+        JsonPlaceholderAPI.printAllComments(userId);
+
         //Завдання 3
-        JsonPlaceholderAPI.openUserTasks(1);
+        System.out.println(String.format("Перевірка завдання 3\nВивід невиконаних завдань користувача з id %d", userId));
+        JsonPlaceholderAPI.openUserTasks(userId);
     }
 }
